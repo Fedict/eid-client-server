a8e8f5316c6eeefd8423d3495bc0c296d2fb1432
Jan Van den Bergh <jan.vandenbergh@aca-it.be>
2018-01-16 11:17:26 +0100

    Apply new copyright.

--- eid-client-server-shared/src/main/java/be/bosa/eid/client_server/shared/message/IdentityDataMessageSemanticValidator.java
+++ eid-client-server-shared/src/main/java/be/bosa/eid/client_server/shared/message/IdentityDataMessageSemanticValidator.java
@@ -1,26 +1,23 @@
 /*
- * eID Applet Project.
- * Copyright (C) 2008-2009 FedICT.
- * Copyright (C) 2014 e-Contract.be BVBA.
+ * eID Client - Server Project.
+ * Copyright (C) 2018 - 2018 BOSA.
  *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License version
- * 3.0 as published by the Free Software Foundation.
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License version 3.0 as published by
+ * the Free Software Foundation.
  *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * This software is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, see
- * http://www.gnu.org/licenses/.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this software; if not, see https://www.gnu.org/licenses/.
  */
 
 package be.bosa.eid.client_server.shared.message;
 
 import be.bosa.eid.client_server.shared.protocol.SemanticValidator;
-import be.bosa.eid.client_server.shared.protocol.SemanticValidatorException;
 
 /**
  * Semantic validator implementation of the IdentityDataMessage.
@@ -29,7 +26,7 @@ import be.bosa.eid.client_server.shared.protocol.SemanticValidatorException;
  */
 public class IdentityDataMessageSemanticValidator implements SemanticValidator<IdentityDataMessage> {
 
-	public void validate(IdentityDataMessage object) throws SemanticValidatorException {
+	public void validate(IdentityDataMessage object) {
 		int expectedSize = object.identityFileSize;
 		if (object.addressFileSize != null) {
 			expectedSize += object.addressFileSize;
